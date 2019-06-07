904fe96d7f077b70d136a603718545af92535809
bmoraeshub <51484001+bmoraeshub@users.noreply.github.com>
2019-06-07 11:49:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'status' : 'Rodando...'})
+    return jsonify({'status' : 'Rodando... nao consegue'})
+
+@app.route('/nova-rota')
+def nova_rota():
+    return jsonify({'rota' : 'rota TRE'})
 
 app.run(host='0.0.0.0', port=8080)
